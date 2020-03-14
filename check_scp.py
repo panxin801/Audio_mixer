fa873f406c769c400b383a769248449c47f25735
panxin801 <1457353243@qq.com>
2020-03-14 15:04:15 +0800

    update check_scp

--- check_scp.py
+++ check_scp.py
@@ -11,17 +11,18 @@ needFix = sys.argv[2]
 baseFile = os.path.join(currentPath, baseFile)
 needFix = os.path.join(currentPath, needFix)
 
-baseDict = {}
-for line in open(baseFile, "rt").readlines():
-    uttid, filepath = line.strip().split(" ")
-    baseDict.update({uttid: filepath})
+searchDict = {}
+for line in open(needFix, "rt", encoding="utf-8"):
+    uttid, text = line.split(" ", 1)
+    text = text.strip()
+    searchDict.update({uttid: text})
 
 writeFile = needFix + "new"
 wF = open(writeFile, "wt", encoding="utf-8")
-for line in open(needFix, "rt", encoding="utf-8"):
-    line = line.strip()
-    uttid, text = line.split(" ", 1)
-    if uttid in baseDict:
-        wF.write("%s %s\n" % (uttid, text))
+for line in open(baseFile, "rt").readlines():
+    uttid = line.strip().split(" ")[0]
+    if uttid in searchDict:
+        wF.write("%s %s\n" % (uttid, searchDict.get(uttid)))
+
 wF.close()
 print("Done!")
