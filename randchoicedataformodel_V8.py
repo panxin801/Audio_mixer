2b76151cbced4673ef988628924f4fb046c0e163
panxin801 <1457353243@qq.com>
2020-03-16 11:37:02 +0800

    update randchoice v8

--- randchoicedataformodel_V8.py
+++ randchoicedataformodel_V8.py
@@ -2,6 +2,27 @@ import os
 import sys
 import random
 
+
+def write_new_file(currentPath, processFileName, textContext):
+    num = 0
+    tmpSave = os.path.join(currentPath, processFileName + "_new")
+    tmpRead = os.path.join(currentPath, processFileName)
+    with open(tmpSave, "wt") as f:
+        for line in open(tmpRead, "rt").readlines():
+            line = line.strip()
+            uttid, context = line.split(" ")
+            if uttid in textContext.keys():
+                f.write("%s %s\n" % (uttid, context))
+                num += 1
+    print("%s write done." % (tmpSave))
+    if num == len(textContext.keys()):
+        os.remove(tmpRead)
+        os.rename(tmpSave, tmpRead)
+    else:
+        print("%s dont have same lines with text." % (tmpRead))
+        sys.exit(1)
+
+
 if __name__ == "__main__":
     currentPath = r"/home/panxin/kaldi/egs/aishell2_kefu/s5/data/train"
     savePath = r"/home/panxin/kaldi/egs/aishell2_kefu/s5/data"
@@ -22,20 +43,24 @@ if __name__ == "__main__":
             utt = str(utt)
             text = str(text)
             textContext.update([(utt, text)])
-        except:
+        except Exception:
             empty = True
             print(line)
     # Done text dict make
 
     # maybe this one is optional
-    tmpSave = os.path.join(currentPath, "text_new")
-    with open(tmpSave, "wt", encoding="utf-8") as f:
-        for key in textContext.keys():
-            f.write("%s %s\n" % (key, textContext.get(key)))
-
     if empty:
+        tmpSave = os.path.join(currentPath, "text_new")
+        with open(tmpSave, "wt", encoding="utf-8") as f:
+            for key in textContext.keys():
+                f.write("%s %s\n" % (key, textContext.get(key)))
         print("Blank line occurs!!!")
-        sys.exit(1)
+        tmpRead = os.path.join(currentPath, "text")
+        os.remove(tmpRead)
+        os.rename(tmpSave, tmpRead)
+
+        write_new_file(currentPath, "utt2spk", textContext)
+        write_new_file(currentPath, "wav.scp", textContext)
 
     readFilename = os.path.join(currentPath, "wav.scp")
     fileContext = {}
@@ -43,7 +68,7 @@ if __name__ == "__main__":
         line = line.strip()
         utt, path = line.split(" ")
         utt = str(utt)
-        fileContext.update([(utt, path)])
+        fileContext.update({utt: path})
     # Done wav dict make
 
     utt2spkContext = {}
@@ -54,7 +79,7 @@ if __name__ == "__main__":
         utt, spk = line.split(" ")
         utt = str(utt)
         spk = str(spk)
-        utt2spkContext.update([(utt, spk)])
+        utt2spkContext.update({utt: spk})
     # Done utt2spk dict make
 
     fileKeys = list(fileContext.keys())
